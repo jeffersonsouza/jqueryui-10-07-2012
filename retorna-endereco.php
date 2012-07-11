f94a1bfd70bb92f79e90879cee923c885c076b2b
Jefferson Souza <hsinfo@gmail.com>
2012-07-10 21:17:09 -0300

    Busca endereços com ajax

---
+++ retorna-endereco.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+$xml = simplexml_load_file("http://cep.republicavirtual.com.br/web_cep.php?cep=" . $_GET['cep']);
+
+if($xml->resultado == 1){
+	$endereco = "<h4>Endereço</h4>";
+	$endereco .= "<p>" . $xml->tipo_logradouro . ' ' . $xml->logradouro . "</p>";
+	$endereco .= "<p>" . $xml->bairro . ' - ' . $xml->cidade . ' - ' . $xml->uf . "</p>";
+	$endereco .= "<p>CEP.:" . $_GET['cep'] . "</p>";
+	
+	echo $endereco;
+} else {
+	echo 'O cep informado não está cadastrado em nossa base';
+}
